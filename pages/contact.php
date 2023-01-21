a770970dc22a9a528236e8cb375a415f40cc5194
fishkaoff <gameacc44@yandex.ru>
2023-01-21 13:32:12 +0500

    Added Contact page and attractions

--- pages/contact.php
+++ pages/contact.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Diagoona Template - Contact page</title>
+    <title>UfAttr - Contact page</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet" /> <!-- https://fonts.google.com/ -->
     <?php include("../blocks/paths.php");?>
 
@@ -19,20 +19,20 @@
                 <div class="tm-col-left"></div>
                 <main class="tm-col-right tm-contact-main"> <!-- Content -->
                     <section class="tm-content tm-contact">
-                        <h2 class="mb-4 tm-content-title">Contact Us</h2>
-                        <p class="mb-85">Etiam et egestas arcu. Fusce congue quis elit vitae commodo. Cras neque mauris, vehicula in ipsum sit amet, faucibus aliquam arcu.</p>
-                        <form id="contact-form" action="" method="POST">
+                        <h2 class="mb-4 tm-content-title">Связаться с нами</h2>
+                        <p class="mb-85">Ниже вы можете отправить нам сообщение с вашими жалобами, предложениями и идеями. Мы открыты для вас!</p>
+                        <form id="contact-form" action="" meEtiam et egestas arcu. Fusce congue quis elit vitae commodo. Cras neque mauris, vehicula in ipsum sit amet, faucibus aliquam arcuthod="POST">
                             <div class="form-group mb-4">
-                                <input type="text" name="name" class="form-control" placeholder="Name" required="" />
+                                <input type="text" name="name" class="form-control" placeholder="Имя" required="" />
                             </div>
                             <div class="form-group mb-4">
                                 <input type="email" name="email" class="form-control" placeholder="Email" required="" />
                             </div>
                             <div class="form-group mb-5">
-                                <textarea rows="6" name="message" class="form-control" placeholder="Message..." required=""></textarea>
+                                <textarea rows="6" name="message" class="form-control" placeholder="Сообщение..." required=""></textarea>
                             </div>
                             <div class="text-right">
-                                <button type="submit" class="btn btn-big btn-primary">Send It</button>
+                                <button type="submit" class="btn btn-big btn-primary">Отправить</button>
                             </div>
                         </form>
                     </section>
